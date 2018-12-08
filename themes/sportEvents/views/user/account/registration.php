618604ad93ae5f018b7d3984c2a04382f115e43e
zub.s <zub.s@novaposhta.ua>
2018-12-08 17:17:15 +0200

    registration form

--- themes/sportEvents/views/user/account/registration.php
+++ themes/sportEvents/views/user/account/registration.php
@@ -4,135 +4,145 @@ $this->breadcrumbs = [Yii::t('UserModule.user', 'Sign up')];
 ?>
 
 <?php $this->widget('yupe\widgets\YFlashMessages'); ?>
-
-<script type='text/javascript'>
-    $(document).ready(function () {
-        function str_rand(minlength) {
-            var result = '';
-            var words = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-            var max_position = words.length - 1;
-            for (i = 0; i < minlength; ++i) {
-                position = Math.floor(Math.random() * max_position);
-                result = result + words.substring(position, position + 1);
-            }
-            return result;
-        }
-
-        $('#generate_password').click(function () {
-            var pass = str_rand($(this).data('minlength'));
-            $('#RegistrationForm_password').attr('type', 'text');
-            $('#RegistrationForm_password').val(pass);
-            $('#RegistrationForm_cPassword').val(pass);
-        });
-    })
-</script>
-
-<?php $form = $this->beginWidget(
-    'bootstrap.widgets.TbActiveForm',
-    [
-        'id' => 'registration-form',
-        'type' => 'vertical',
-        'htmlOptions' => [
-            'class' => 'well',
-        ]
-    ]
-); ?>
-
-<?= $form->errorSummary($model); ?>
-
-<?php if (!$this->module->generateNickName) : ?>
-    <div class='row'>
-        <div class="col-sm-6">
-            <?= $form->textFieldGroup($model, 'nick_name'); ?>
-        </div>
-    </div>
-<?php endif; ?>
-
-<div class='row'>
-    <div class="col-sm-6">
-        <?= $form->textFieldGroup($model, 'email'); ?>
-    </div>
-</div>
-
-<div class='row'>
-    <div class="col-sm-6">
-        <?= $form->passwordFieldGroup($model, 'password'); ?>
-    </div>
-</div>
-
-<div class='row'>
-    <div class="col-sm-6">
-        <?= $form->passwordFieldGroup($model, 'cPassword'); ?>
-    </div>
-    <div class="col-sm-4 form-group" style="padding-top: 25px;">
-        <?php $this->widget(
-            'bootstrap.widgets.TbButton',
-            [
-                'label' => Yii::t('UserModule.user', 'Generate password'),
-                'htmlOptions' => [
-                    'id' => 'generate_password',
-                    'data-minlength' => $this->module->minPasswordLength
-                ],
-            ]
-        ); ?>
-    </div>
-</div>
-
-<?php if ($module->showCaptcha && CCaptcha::checkRequirements()): { ?>
+<div style="background-color: #f8f8f8; padding: 20px;">
     <div class="row">
-        <div class="col-xs-4">
-            <?= $form->textFieldGroup(
-                $model,
-                'verifyCode',
-                ['hint' => Yii::t('UserModule.user', 'Please enter the text from the image')]
-            ); ?>
-        </div>
-        <div class="col-xs-4">
-            <?php $this->widget(
-                'CCaptcha',
+        <div class="col-sm-6 col-sm-offset-3">
+            <div class='row'>
+            <div class="col-sm-4 col-sm-offset-4">
+                <button type="submit" class="btn btn-primary">Log in with Facebook</button>
+            </div>
+            </div>
+            <?php $form = $this->beginWidget(
+                'bootstrap.widgets.TbActiveForm',
                 [
-                    'showRefreshButton' => true,
-                    'imageOptions' => [
-                        'width' => '150',
-                    ],
-                    'buttonOptions' => [
-                        'class' => 'btn btn-default',
-                    ],
-                    'buttonLabel' => '<i class="glyphicon glyphicon-repeat"></i>',
+                    'id' => 'registration-form',
+                    'type' => 'vertical',
+                    'htmlOptions' => [
+                        'enctype' => 'multipart/form-data'
+//                        'class' => 'well',
+                    ]
                 ]
             ); ?>
-        </div>
-    </div>
-<?php } endif; ?>
 
-<div class="row">
-    <div class="col-xs-12">
-        <?php $this->widget(
-            'bootstrap.widgets.TbButton',
-            [
-                'buttonType' => 'submit',
-                'context' => 'primary',
-                'label' => Yii::t('UserModule.user', 'Sign up'),
-            ]
-        ); ?>
+            <?= $form->errorSummary($model); ?>
+
+            <div class='row'>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'first_name'); ?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'last_name'); ?>
+                </div>
+            </div>
+            <div class='row'>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'email'); ?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'phone'); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <?php $this->widget('application.modules.Event.widgets.SeparateDateWidget',[
+                        'model' => $model,
+                        'attribute' => 'birthday']
+                    );?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'country', [
+                        'widgetOptions' => [
+                            'htmlOptions' => [
+                                    'value' => Yii::t('UserModule.user', 'Україна')
+                            ],
+                            ]
+                    ]);?>
+
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'city');?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <?= $form->dropDownListGroup(
+                        $model,
+                        'gender',
+                        [
+                            'widgetOptions' => [
+                                'data'        => $model->getGenderList(),
+                                'htmlOptions' => [
+                                    'class'               => 'popover-help',
+                                    'data-original-title' => $model->getAttributeLabel('gender'),
+                                    'data-content'        => $model->getAttributeDescription('gender'),
+                                ],
+                            ],
+                        ]
+                    ); ?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->dropDownListGroup(
+                        $model,
+                        't_shirt_size',
+                        [
+                            'widgetOptions' => [
+                                'data'        => array_merge([''], $model->getTShirtSizeList()),
+                                'htmlOptions' => [
+                                    'class'               => 'popover-help',
+                                    'data-original-title' => $model->getAttributeLabel('t_shirt_size'),
+                                    'data-content'        => $model->getAttributeDescription('t_shirt_size'),
+                                    'selected' => 'L'
+                                ],
+                            ],
+                        ]
+                    ); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'club');?>
+                </div>
+                <div class="col-sm-6">
+                    <?= $form->textFieldGroup($model, 'team');?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <?= $form->textFieldGroup($model, 'alternative_contact'); ?>
+                </div>
+            </div>
+            <div class="row">
+            <div class="row">
+                <div class="col-sm-12">
+                    <?= $form->fileFieldGroup(
+                        $model,
+                        'image',
+                        [
+                            'widgetOptions' => [
+                                'htmlOptions' => [
+                                    'onchange' => 'readURL(this);',
+                                    'style' => 'background-color: inherit;',
+                                ],
+                            ],
+                        ]
+                    ); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <?php $this->widget(
+                        'bootstrap.widgets.TbButton',
+                        [
+                            'buttonType' => 'submit',
+                            'context' => 'primary',
+                            'label' => Yii::t('UserModule.user', 'Sign up'),
+                        ]
+                    ); ?>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-
-<?php if (Yii::app()->hasModule('social')): { ?>
-    <hr/>
-    <div class="row">
-        <div class="col-xs-12">
-            <?php $this->widget(
-                'vendor.krupni.yii-eauth.EAuthWidget',
-                [
-                    'action' => '/social/login',
-                    'predefinedServices' => ['google', 'facebook', 'vkontakte', 'twitter', 'github', 'odnoklassniki'],
-                ]
-            ); ?>
-        </div>
-    </div>
-<?php } endif; ?>
-
 <?php $this->endWidget(); ?>
-<!-- form -->
-
